03d3fdc6feded7fbf2bf28ae421239c11fcd0469
Elena Butakova <elenabutakova567@gmail.com>
2023-05-28 16:40:04 +0500

    add suot

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -5,7 +5,7 @@ public class Test {
 
     @
     void newTest(){
-        System.out.println("this is the best code");
+        System.out.println("this is the best code121212212");
         Assertions.assertTrue(3>2); //test
     }
 }
